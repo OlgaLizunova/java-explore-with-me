3b8fccf8719575709f130bbe0d66e67ed148d867
Olga Lizunova <lizunoff.olga@yandex.ru>
2023-10-21 02:10:03 +0300

    Added InvalidDateRangeException in the stats

--- stats/server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
+++ stats/server/src/main/java/ru/practicum/stats/server/controller/StatsController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.stats.dto.EndpointHitDto;
 import ru.practicum.stats.dto.StatsDto;
+import ru.practicum.stats.server.exceptions.InvalidDateRangeException;
 import ru.practicum.stats.server.service.StatsService;
 
 import javax.validation.Valid;
@@ -31,18 +32,25 @@ public class StatsController {
 
     @GetMapping("/stats")
     public ResponseEntity<List<StatsDto>> getViewStats(
-            @RequestParam(value = "start")
+            @RequestParam(value = "start", required = false)
             @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime start,
-            @RequestParam(value = "end")
+            @RequestParam(value = "end", required = false)
             @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss") LocalDateTime end,
             @RequestParam(value = "uris", required = false) List<String> uris,
             @RequestParam(value = "unique", defaultValue = "false", required = false) boolean isUnique
     ) {
+        checkStartEndRanges(start, end);
         if (uris == null) {
             uris = Collections.emptyList();
         }
         log.info("receive GET request for return ViewStats from={}, to={}, uris={}, uniq={}", start, end, uris, isUnique);
         List<StatsDto> viewStatsDto = statsService.getViewStats(start, end, uris, isUnique);
         return new ResponseEntity<>(viewStatsDto, HttpStatus.OK);
+    }
+
+    private void checkStartEndRanges(LocalDateTime start, LocalDateTime end) {
+        if (end == null || start == null || end.isBefore(start)) {
+            throw new InvalidDateRangeException("Date range is invalid");
+        }
     }
 }
